b46dfc5cb928831c283c268ce19e4d0b00c74ba8
ahmedhamdy <a01022395595@gmail.com>
2025-03-22 06:24:22 +0300

    creat project

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+// import React from "react";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Profile from "./pages/Profile";
+// import Navbar from "./components/Navbar";
+// import CreatePost from "./pages/CreatePost";
+// import Chat from "./pages/Chat";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>social media project</h1>
+    {/* <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/create-post" element={<CreatePost />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </Router> */}
+    </>
+  );
+}
+
+export default App;
