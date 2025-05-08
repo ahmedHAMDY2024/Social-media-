277f0bbbb8db63af3cb4765917f22e789e4aec42
ahmedhamdy <a01022395595@gmail.com>
2025-05-08 18:05:48 +0300

    conferm build

--- src/Componant/pages/Home.jsx
+++ src/Componant/pages/Home.jsx
@@ -4,6 +4,15 @@ function Home(){
     return(
         <div className="Home">
             <div id="carouselExampleControlsNoTouching" className="carousel slide" data-bs-touch="false">
+            {/* user data */}
+            <div className='userData'>
+                <h4 className='userName'>ahmed hamdy</h4>
+                <img src="https://scontent.fruh6-1.fna.fbcdn.net/v/t39.30808-6/494643498_537270576117187_7501532892422331333_n.jpg?stp=dst-jpg_p526x296_tt6&_nc_cat=101&ccb=1-7&_nc_sid=833d8c&_nc_ohc=SWumsd5iDEUQ7kNvwFK1PLu&_nc_oc=AdkK13Z_vw9UJnAB_S7awqdu7N7VBSHYHP3Fz43hNAfq2VKsPa80REu1Td3_6m0VtiE&_nc_zt=23&_nc_ht=scontent.fruh6-1.fna&_nc_gid=TDz_I7aWHQbAbBwC4E0VAg&oh=00_AfEH76T28wpLt1DGlM3ONtPz69D33OYyWL6NH3qdEWSaHw&oe=68192629" alt="" className='userImg'/>
+            </div>
+            {/* descreption of post */}
+            <div className='descreptionOfPost'>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, sunt accusamus illum sit suscipit a rem illo eius consequatur aliquid, doloremque aperiam? Perspiciatis exercitationem nam libero minus. Magnam, commodi natus?</p>
+            </div>
                 <div className="carousel-inner">
                     <div className="carousel-item active">
                     <img src="../src/Componant/IMG_7915.JPG" className="d-block w-100 imgPost" alt="..."/>
@@ -11,17 +20,38 @@ function Home(){
                 </div>
                 {/* add comment or like */}
                 <div className='comment-like'>
-                   <span className='AddLike'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-thumbs-up" viewBox="0 0 16 16">
-                            <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2 2 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a10 10 0 0 0-.443.05 9.4 9.4 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a9 9 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.2 2.2 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.9.9 0 0 1-.121.416c-.165.288-.503.56-1.066.56z"/>
+                   <span className='AddLike'> Like</span>
+                    <span className='AddComeent'>Comment </span>
+                </div>
+                {/* conferm comment */}
+                <div className='comment'>
+                    <input type="text" placeholder='Text' className='inputComment' />
+                    <button className='btnCommeny'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send-fill" viewBox="0 0 16 16">
+                          <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z"/>
                         </svg>
-                    </span>
-                    <span className='AddComeent'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-right-dots" viewBox="0 0 16 16">
-                            <path d="M2 1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h9.586a2 2 0 0 1 1.414.586l2 2V2a1 1 0 0 0-1-1zm12-1a2 2 0 0 1 2 2v12.793a.5.5 0 0 1-.854.353l-2.853-2.853a1 1 0 0 0-.707-.293H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z"/>
-                            <path d="M5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
-                        </svg>
-                    </span>
+                    </button>
+                </div>
+            </div>
+            <div id="carouselExampleControlsNoTouching" className="carousel slide" data-bs-touch="false">
+            {/* user data */}
+            <div className='userData'>
+                <h4 className='userName'>ahmed hamdy</h4>
+                <img src="https://scontent.fruh6-1.fna.fbcdn.net/v/t39.30808-6/494643498_537270576117187_7501532892422331333_n.jpg?stp=dst-jpg_p526x296_tt6&_nc_cat=101&ccb=1-7&_nc_sid=833d8c&_nc_ohc=SWumsd5iDEUQ7kNvwFK1PLu&_nc_oc=AdkK13Z_vw9UJnAB_S7awqdu7N7VBSHYHP3Fz43hNAfq2VKsPa80REu1Td3_6m0VtiE&_nc_zt=23&_nc_ht=scontent.fruh6-1.fna&_nc_gid=TDz_I7aWHQbAbBwC4E0VAg&oh=00_AfEH76T28wpLt1DGlM3ONtPz69D33OYyWL6NH3qdEWSaHw&oe=68192629" alt="" className='userImg'/>
+            </div>
+            {/* descreption of post */}
+            <div className='descreptionOfPost'>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, sunt accusamus illum sit suscipit a rem illo eius consequatur aliquid, doloremque aperiam? Perspiciatis exercitationem nam libero minus. Magnam, commodi natus?</p>
+            </div>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                    <img src="../src/Componant/IMG_7915.JPG" className="d-block w-100 imgPost" alt="..."/>
+                    </div>
+                </div>
+                {/* add comment or like */}
+                <div className='comment-like'>
+                   <span className='AddLike'> Like</span>
+                    <span className='AddComeent'>Comment </span>
                 </div>
                 {/* conferm comment */}
                 <div className='comment'>
